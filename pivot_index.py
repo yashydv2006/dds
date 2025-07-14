defc87ea671736065bc425dbc3b7d30818f299d1
yashydv2006 <yashydv2610@gmail.com>
2025-07-14 08:59:10 +0530

    Update pivot_index.py

--- pivot_index.py
+++ pivot_index.py
@@ -4,3 +4,18 @@ class Solution:
             if sum(nums[:i]) == sum(nums[i+1:]):
                 return i
         return -1        
+
+
+
+#2 
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        t = sum(nums)
+        l_t = 0
+        for i in range(len(nums)):
+           r_t = t - l_t - nums[i]
+           if r_t == l_t:
+               return i
+           l_t += nums[i]
+        return -1         
